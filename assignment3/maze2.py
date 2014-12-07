343fa73da6887d2c7795b63e5e6e2a8746682062
scagi1 <scagi1@student.monash.edu>
2014-12-07 16:30:12 +1100

    random maze generator (mostly) working

---
+++ assignment3/maze2.py
@@ -0,0 +1,96 @@
+
+from random import randrange
+from random import shuffle
+
+class Tile():
+    def __init__(self, x, y):
+        
+        self.column = x
+        self.row = y
+        
+        self.connected = False
+        
+        self.edges = [False, False, False, False]
+        
+    def Left(self):
+        return self.edges[0]
+    def Right(self):
+        return self.edges[1]
+    def Up(self):
+        return self.edges[2]
+    def Down(self):
+        return self.edges[3]
+    
+class Maze():
+    def __init__(self, size):
+        self.size = size
+        self.maze = []
+        
+        
+        for y in range(size):
+            level = []
+            for x in range(size):
+                level.append(Tile(x, y))
+            self.maze.append(level)
+            
+             
+        self.start = self.maze[0][randrange(size)]
+        self.start.connected = True
+        
+        
+    def generate(self):
+        #[left, right, up, down]
+        X = [0, 0, 1, -1]
+        Y = [-1, 1, 0, 0]
+        order = [0, 1, 2, 3]
+        
+        nodes = [self.start]
+        
+        while len(nodes) > 0:
+            
+            current = nodes[-1]
+            
+            randorder = order
+            shuffle(randorder)
+            
+            nextTile = None
+            for side in randorder:
+                nextRow = current.row + X[side]
+                nextColumn = current.column + Y[side]
+                if (nextRow >= 0 and nextRow < self.size and nextColumn >= 0 and nextColumn < self.size) and (not (self.maze[nextRow][nextColumn].connected)):
+                    nextTile = self.maze[nextRow][nextColumn]
+                    
+                    current.edges[side] = nextTile
+                    if side % 2 == 0:
+                        nextTile.edges[side+1] = current
+                    else:
+                        nextTile.edges[side-1] = current
+                    break
+                
+            if not (nextTile is None):
+                nextTile.connected = True
+                nodes.append(nextTile)
+            else:
+                nodes.pop()
+                
+                
+    def display(self):
+        for row in self.maze:
+            a = []
+            for column in row:
+                b = ""
+                for i in column.edges:
+                    b = b + str(i)[0]
+                a.append(b)
+            print a
+                
+            
+
+
+
+if __name__ == '__main__':
+    print "start"
+    x = Maze(5)
+    x.generate()
+    x.display()
+    print"done"
