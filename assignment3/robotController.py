da3eb5681c03657d4ef8f21cae2c3a897eb77189
Jarrad Whitaker <jarrad.whitaker@gmail.com>
2014-12-10 14:28:56 +1100

    first implementation of the robot controller

--- assignment3/robotController.py
+++ assignment3/robotController.py
@@ -1,4 +1,21 @@
+import fcode
+
 class RobotController():
-	def __init__(self, robot):
+	def __init__(self, robot, maze):
 		self.robot = robot
+		self.maze = maze
+		self.fCode = None
+		self.robotEnv = fcode.build_global_env()
+		self.robotEnv.update({
+			'move':    self.robot.move
+			'turn':	   lambda x: self.turn(x)
+			'detect-wall': self.robot.detectWall,
+			'detect-goal': self.detectGoal,
+		})
 		
+	def run(self):
+		fcode.eval(self.fCode)
+	
+	def detectGoal(self):
+		self.robot.detectGoal(self.maze)
+	
