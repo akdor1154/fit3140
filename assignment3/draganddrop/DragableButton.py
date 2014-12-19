2dfb140a69daffcfe0757220307be3535feabf0d
scagi1 <scagi1@student.monash.edu>
2014-12-19 17:34:39 +1100

    scaling

--- assignment3/draganddrop/DragableButton.py
+++ assignment3/draganddrop/DragableButton.py
@@ -6,7 +6,6 @@ Created on Oct 24, 2012
 
 
 from DragNDropWidget import DragNDropWidget
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import *
@@ -36,49 +35,7 @@ class DragableButton(Button, DragNDropWidget):
                               drag_opacity=self.drag_opacity,
                               drop_func=self.drop_func,
                               remove_on_drag=self.remove_on_drag)
-"""     
-
-class FArgument(Widget):
-    def __init__(self, text, **kwargs):
-        super(self.__class__,self).__init__(**kwargs)
-        self.width = 80
-        self.height = 50
-        self.label = Label(text=text)
-        with self.canvas:
-            Color(0.6,0.4,0.4)
-            self.bg = Rectangle(size=self.size, pos=self.pos)
-        self.add_widget(self.label)
-        
-    def on_size(self, instance, value):
-        self.bg.size = self.size
-        
-    def on_pos(self, instance, value):
-        self.bg.pos = self.pos
-        self.label.pos = self.pos
-        
-        
-    
-class FName(Widget):
-    def __init__(self, text, **kwargs):
-        super(self.__class__,self).__init__(**kwargs)
-        self.width = 80
-        self.height = 80
-        self.label = Label(text=text)
-        with self.canvas:
-            Color(0.4,0.4,0.4)
-            self.bg = Rectangle(size=self.size, pos=self.pos)
-        self.add_widget(self.label)
-        
-    
-    def on_size(self, instance, value):
-        self.bg.size = self.size
-        
-    def on_pos(self, instance, value):
-        self.bg.pos = self.pos
-        self.label.pos = self.pos
-       
-     
-    
+"""        
 class DragableLayout(BoxLayout, DragNDropWidget):
     '''
     classdocs
@@ -98,12 +55,10 @@ class DragableLayout(BoxLayout, DragNDropWidget):
         
         print (self.parent, "<<<<<<<<<<<PARENT")
         
-    def makeFunction(self, functionName, nArgs):
-        functionName = FName(text=str(functionName))
-        
-        self.mainLayout.add_widget(functionName)
-        for _ in range(nArgs):
-            l = FArgument(text="drop arguments here")
+    def makeFunction(self, FName, Nargs):
+        self.mainLayout.add_widget(Button(text=str(FName)))
+        for _ in range(Nargs):
+            l = Button(text="drop arguments here")
             dropZones.append(l)
             #with l.canvas:
             #    Rectangle(pos=l.pos, size=l.size, color=(0,0,1))
@@ -122,6 +77,21 @@ class DragableLayout(BoxLayout, DragNDropWidget):
         fBlockParent.remove_widget(argumentToReplace)
         self.parent.remove_widget(self)
         fBlockParent.add_widget(self, index=indexOfArgument)
+        self.size_hint = (1, 3)
+        
+        p = self.parent
+        mult = 2
+        while (p is not None):
+            try:
+                PH = p.size_hint
+                if not ((PH[0] is None) or (PH[1] is None)):
+                        self.parent.size_hint = (PH[0], PH[1]*mult)
+                p = p.parent
+                mult = mult * 0.75
+                if mult < 1:
+                    mult = 1.1
+            except:
+                break
         
         
 
@@ -129,7 +99,8 @@ class DragableLayout(BoxLayout, DragNDropWidget):
         #self.canvas.ask_update
         #self.parent.canvas.ask_update
         
-        #self.do_layout()
+        self.parent.canvas.ask_update
+        self.canvas.ask_update
         
     def __deepcopy__(self, dumb):
         return DragableLayout(droppable_zone_objects=self.droppable_zone_objects,
