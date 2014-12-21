3cca4693a58b100a3fd079bbe3f9def3e5bc0ad1
Jarrad Whitaker <akdor1154@gmail.com>
2014-12-21 19:35:03 +1100

    DnD prototype kinda sorta complete yay

--- assignment3/testUI/test.py
+++ assignment3/testUI/test.py
@@ -8,54 +8,65 @@ from kivy.uix.label import Label
 from kivy.properties import NumericProperty, OptionProperty, VariableListProperty
 from kivy.graphics import *
 
+from pdb import set_trace
+
+
+from DragNDropWidget import DragNDropWidget
+
 class FArgument(Widget):
-	def __init__(self, text, **kwargs):
+	def __init__(self, text="__", **kwargs):
+		self.name = text
 		super(self.__class__,self).__init__(**kwargs)
 		with self.canvas:
 			Color(0.6,0.4,0.4)
 			self.bg = Rectangle(size=self.size, pos=self.pos)
-		self.label = Label(text=text, padding=[-FLayout.blockIndent, 0], valign="middle")
+		self.label = Label(text=self.name, padding=[-FLayout.blockIndent, 0], valign="middle")
 		self.add_widget(self.label)
-		self.bind(size=self.on_size, pos=self.on_pos)
+		self.bind(size=self.redoLayout, pos=self.redoLayout)
 		
-	def on_size(self, instance, value):
+	def redoLayout(self, instance, value):
 		self.bg.size = self.size
 		self.label.size = self.size
 		self.label.text_size = self.size
 		
-	def on_pos(self, instance, value):
 		self.bg.pos = self.pos
 		self.label.pos = self.pos
 		
+	
+	def __repr__(self):
+		return self.name
 		
 	
 class FName(Widget):
 	def __init__(self, text, **kwargs):
+		self.name = text
 		super(self.__class__,self).__init__(**kwargs)
 		with self.canvas:
 			Color(0.4,0.4,0.4, 0.4)
 			self.bg = Rectangle(size=self.size, pos=self.pos)
-		self.label = Label(text=text, padding=[-FLayout.blockIndent, 0])
+		self.label = Label(text=self.name, padding=[-FLayout.blockIndent, 0])
 		self.add_widget(self.label)
-		self.bind(size=self.on_size, pos=self.on_pos)
+		self.bind(size=self.redoLayout, pos=self.redoLayout)
 		
 	
-	def on_size(self, instance, value):
+	def redoLayout(self, instance, value):
 		self.bg.pos = (self.pos[0], self.pos[1]-(self.parent.calculateHeight()-self.height))
 		self.bg.size = (self.parent.calculateWidth(), self.parent.calculateHeight())
 		self.label.size = self.size
 		self.label.text_size = self.size
 		
-	def on_pos(self, instance, value):
 		self.bg.pos = (self.pos[0], self.pos[1]-(self.parent.calculateHeight()-self.height))
 		self.label.pos = self.pos
+		
 
-
-
-class FLayout(Layout):
+	def __repr__(self):
+		return self.name
+		
+class FLayout(Layout, DragNDropWidget):
 	'''Box layout class. See module documentation for more information.
 	'''
 
+	droppable_zone_objects = []
 	nameHeight = 50
 	argHeight = nameHeight
 	blockWidth = 100
@@ -84,25 +95,77 @@ class FLayout(Layout):
 
 	'''
 
-	def __init__(self, fName=None, fArgs=[], **kwargs):
+	def __init__(self, fNameString=None, fArgStrings=[], numArgs=0, **kwargs):
 		self.neededWidthCache = None
 		self.neededHeightCache = None
+		self.fName = None
+		self.fArgs = []
 		super(FLayout, self).__init__(**kwargs)
 		self.bind(
 			spacing=self._trigger_layout,
 			padding=self._trigger_layout,
-			children=self._trigger_layout,
+			children=self._update_children,
 			parent=self._trigger_layout,
 			size=self._trigger_layout,
-			pos=self._trigger_layout)
+			pos=self._trigger_layout
+		)
 			
-		if fName:
-			self.add_widget(FName(fName))
+		if fNameString:
+			self.add_widget(FName(fNameString))
 		
-		if fArgs:
-			for arg in fArgs:
-				self.add_widget(FArgument(arg))
+		if numArgs and fArgStrings and numArgs < len(fArgStrings):
+			raise ValueError('you passed more arguments that you said your function could take')
+		
+		self.numArgs = max(numArgs, len(fArgStrings))
+		for i in range(self.numArgs):
+			try:
+				self.fArgs.append(FArgument(fArgStrings[i]))
+			except ValueError:
+				self.fArgs.append(FArgument())
+				
+		map(lambda x: self.add_widget(x, forceAdd=True), self.fArgs)
+				
+		self.drop_func = self.replaceWidget
+		
+	def __repr__(self):
+		return repr(self.fName)+'('+','.join(map(repr,self.fArgs))+')'
+		
+	def isRoot(self):
+		try:
+			return self.parent.__class__ is not FLayout
+		except AttributeError:
+			return True
+			
+	def getRootFLayouts(self):
+		try: return [widget for widget in self.get_root_window().children if widget.__class__ is FLayout]
+		except AttributeError: return []
+			
+	def getRootFLayout(self): #yay recursion
+		return self if self.isRoot else self.parent.getRootFLayout()
+	
+	def _update_children(self, instance, value):
+		self._invalidateSizeCache()
+		self._trigger_layout(instance, value)
+		self.updateDropZones()
+		
+	def updateDropZones(self):
+		#"Functional programming can be concise, logical, and easy-to-read" -- someone who didn't read the next line
+		FLayout.droppable_zone_objects = reduce(lambda listOfDropZones, rootFLayout: listOfDropZones+rootFLayout._getDropZones(), self.getRootFLayouts(), [])
 
+				
+	def _getDropZones(self):
+		dropZones = []
+		for child in self.children:
+			if (
+				(child.__class__ is FName and not self.isRoot())
+			or
+				(child.__class__ is FArgument)
+			):
+				dropZones.append(child)
+			elif child.__class__ is FLayout:
+				dropZones += child._getDropZones()
+		return dropZones
+				
 	def do_layout(self, *largs):
 		# optimize layout by preventing looking at the same attribute in a loop
 		len_children = len(self.children)
@@ -133,7 +196,8 @@ class FLayout(Layout):
 			elif child.__class__ is FArgument:
 				childWidth = self.calculateWidth()-self.blockIndent
 				childHeight = self.argHeight
-				
+			else:
+				raise Exception("tried to add a ",child.__class__,"to an FLayout")
 			
 			
 			childX = selfx + padding_left
@@ -145,9 +209,18 @@ class FLayout(Layout):
 
 			child.x = childX
 			child.y = childY
-			child.width = childWidth
+			if child.__class__ is not FLayout:
+				child.width = childWidth
 			child.height = childHeight
 			y += childHeight + spacing
+		
+		try:
+			self.parent.do_layout()
+		except AttributeError:
+			pass
+			
+		self.width = self.calculateWidth()
+		self.height = self.calculateHeight()
 
 
 	def calculateHeight(self):
@@ -176,26 +249,48 @@ class FLayout(Layout):
 			self.neededWidthCache = max(neededWidths)
 		return self.neededWidthCache
 		
-	def _inavlidateSizeCache(self):
+	def _invalidateSizeCache(self):
 		self.neededHeightCache = None
 		self.neededWidthCache = None
 		try:
 			self.parent._invalidateSizeCache()
 		except AttributeError:
 			pass
-		
-	def add_widget(self, widget, index=0):
-		widget.bind(
-			pos_hint=self._trigger_layout)
-		self._inavlidateSizeCache()
+	
+	def replaceArgument(self, argToAdd, argToReplace):
+		self.fArgs[self.fArgs.index(argToReplace)] = argToAdd
+		childrenIndex = self.children.index(argToReplace)
+		self.remove_widget(argToReplace, forceRemove=True)
+		self.add_widget(argToAdd, childrenIndex, forceAdd=True)
+	
+	def add_widget(self, widget, index=0, forceAdd=False):
+		if widget.__class__ is FName:
+			if self.fName:
+				self.remove_widget(self.fName)
+			self.fName = widget
+			index = -1
+		elif isinstance(widget, (FArgument, FLayout)):
+			if not forceAdd:
+				raise ValueError('you can\'t manually add/remove arguments. Use replaceArgument instead!')
+		widget.bind(pos_hint=self._trigger_layout)
+		self._invalidateSizeCache()
 		return super(FLayout, self).add_widget(widget, index)
 
-	def remove_widget(self, widget):
+	def remove_widget(self, widget, forceRemove=False):
+		if widget.__class__ is FName:
+			self.fName = None
+		elif isinstance(widget, FArgument):
+			if not forceRemove:
+				raise ValueError('you can\'t manually add/remove arguments. Use replaceArgument instead!')
+		elif isinstance(widget, FLayout):
+			if not forceRemove:
+				return self.replaceArgument(FArgument(), widget)
 		widget.unbind(
 			pos_hint=self._trigger_layout)
-		self._inavlidateSizeCache()
+		self._invalidateSizeCache()
 		return super(FLayout, self).remove_widget(widget)
-
+	
+		
 	def setName(self):
 		#TODO:
 		pass
@@ -203,30 +298,63 @@ class FLayout(Layout):
 	def addArgument(self):
 		#TODO:
 		pass
+		
+	def on_touch_down(self, touch):
+
+		if self.fName and self.fName.collide_point(touch.x, touch.y): #we only handle the touch if it's over our Name. Making the Name object handle the touch itself
+														#might seem more sensible but I promise it's not. :)
+			if not self.isRoot():
+				print("need to deparent and stuff")
+			else:
+				print("I AM ROOT")
+				
+			DragNDropWidget.on_touch_down(self, touch)
+		else:
+			#this make the touch bubble to some other widget. maybe touch.bubble() would be a less retarded syntax, Kivy folks?
+			return Widget.on_touch_down(self, touch)
+	
+	def replaceWidget(self, widgetToReplace):
+		
+		if widgetToReplace.__class__ is FName:
+			if widgetToReplace.parent.isRoot():
+				raise Exception('tried to replace the root layout, this should be impossible as it shouldn\'t be a drop target')
+			widgetToReplace = widgetToReplace.parent
+			
+		if widgetToReplace.__class__ in (FArgument, FLayout):
+			fBlockParent = widgetToReplace.parent
+			
+			#nonetype has no parent??????????????
+			
+			
+			self.parent.remove_widget(self)
+			fBlockParent.replaceArgument(self, widgetToReplace)
+
+        
 
 if __name__ == '__main__':
 	class TestApp(App):
 		def build(self):
-			fTest = FLayout(orientation="vertical", pos=(50,50))
 			
-			fSubTest = FLayout()
+			fTest = FLayout("hello()", ["arg1", "arg2"], pos=(50,50))
+			
+			fSubTest = FLayout("subfunctionlonglonglong", ["arg1_1", "arg1_2", "arg1_3"])
 			
 			fSubSubTest = FLayout("subsubfunc()", ["arg3_1", "arg3_2", "arg3_3"])
 			
-			fSubTest.add_widget(FName("subfunctionlonglonglong()"))
-			fSubTest.add_widget(FArgument("arg1_1"))
-			fSubTest.add_widget(fSubSubTest)
-			fSubTest.add_widget(FArgument("arg2_3"))
+			fSubTest.replaceArgument(fSubSubTest, fSubTest.fArgs[1])
 			
-			fTest.add_widget(FName("hello()"))
+			fTest.replaceArgument(fSubTest, fTest.fArgs[0])
 			
-			fTest.add_widget(fSubTest)
-			fTest.add_widget(FArgument("arg2"))
 			
+			print("fTest is root: ",fTest.isRoot())
+			print("fSubTest is root: ",fSubTest.isRoot())
+			print("fSubSubTest is root: ",fSubSubTest.isRoot())
+			
+			print("root layouts:", fSubTest.getRootFLayouts())
 			
 			
 			return fTest
-		
+	
 	TestApp().run()
 	
 
